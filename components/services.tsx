66298017c0590447b55375f4ac3629774e116be9
sohanemon <m.sohanemon@gmail.com>
2022-12-03 15:58:08 +0600

    service card added

--- components/services.tsx
+++ components/services.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Heading from "./ui/heading";
 
 interface Props {}
@@ -6,8 +7,34 @@ const Services: React.FC<Props> = () => {
   return (
     <section>
       <Heading uncolored='Provide awesome' colored='services' />
+      {/* cards section */}
+      {images.map((_) => (
+        <Card src={_.src} />
+      ))}
     </section>
   );
 };
 
 export default Services;
+
+type CardProps = {
+  src: string;
+};
+
+const Card = ({ src }: CardProps) => {
+  return (
+    <div>
+      <div className='overflow-hidden '>
+        <Image src={src} objectFit='cover' layout='responsive' alt='' />
+      </div>
+    </div>
+  );
+};
+
+const images = [
+  require("../assets/images/icons/service1.png"),
+  require("../assets/images/icons/service2.png"),
+  require("../assets/images/icons/service3.png"),
+  require("../assets/images/icons/service4.png"),
+  require("../assets/images/icons/service5.png"),
+];
