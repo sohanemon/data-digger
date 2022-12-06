ef08c9811aa3a8ef1a8952f6f7c3b8702ea2e827
sohanemon <m.sohanemon@gmail.com>
2022-12-06 12:17:44 +0600

    register page done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import MainIcon from "../../components/ui/main-icon";
 
-interface Props {}
+interface Props {
+  reg: boolean;
+}
 
-const Page: React.FC<Props> = () => {
+const Page: React.FC<Props> = ({ reg }) => {
   return (
     <main className='grid place-content-center'>
       <div>
@@ -12,7 +14,9 @@ const Page: React.FC<Props> = () => {
           <MainIcon />
         </div>
         <section className='flex flex-col items-center px-14 border border-gray-400 rounded py-32'>
-          <p className='text-xl font-bold mb-4'>Login With</p>
+          <p className='text-xl font-bold mb-4'>
+            {reg ? "Register" : "Login"} With
+          </p>
           <button className='flex items-center w-80 rounded-full border-gray-400 border p-1 '>
             <div className='overflow-hidden h-8 w-8'>
               <Image
@@ -27,10 +31,13 @@ const Page: React.FC<Props> = () => {
             </p>
           </button>
           <p className='font-medium text-center mt-3'>
-            Don’t have an account?
-            <Link href={"/register"} className='text-blue-600'>
+            {reg ? "Already have an account" : " Don’t have an account?"}
+            <Link
+              href={reg ? "/login" : '"/register"'}
+              className='text-blue-600'
+            >
               {" "}
-              Create an account
+              {reg ? "Login now" : "Create an account"}
             </Link>
           </p>
         </section>
---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+import Page from "../login/page";
+
+interface Props {}
+
+const Register: React.FC<Props> = () => {
+  return (
+    <>
+      <Page reg />
+    </>
+  );
+};
+
+export default Register;
